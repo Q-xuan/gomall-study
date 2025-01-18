de6c952c60c938436a1d223f19eee4a2931c4288
pyu <1391989896@qq.com>
2025-01-18 11:06:30 +0800

    add: 添加注释

--- demo/demo_proto/middleware/middleware.go
+++ demo/demo_proto/middleware/middleware.go
@@ -9,6 +9,7 @@ import (
 )
 
 // 中间件 方便请求前请求后执行一些逻辑 （比如：权限校验
+// clouwego 提供的 middleware 看 hertz-contrib
 func Middleware(next endpoint.Endpoint) endpoint.Endpoint {
 	return func(ctx context.Context, req, resp interface{}) (err error) {
 		begin := time.Now()
